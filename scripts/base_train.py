93aab615fd4914a266d3f03d86ebcb69163dfe9f
Kamsi Rodrick <148686197+RodrickKamsiyonna@users.noreply.github.com>
2026-09-02 13:03:26 +0100

    Update base_train.py

--- scripts/base_train.py
+++ scripts/base_train.py
@@ -73,12 +73,12 @@ parser.add_argument("--warmdown-ratio", type=float, default=0.65, help="ratio of
 parser.add_argument("--final-lr-frac", type=float, default=0.05, help="final LR as fraction of initial LR")
 parser.add_argument("--resume-from-step", type=int, default=-1, help="resume training from this step (-1 = disable)")
 # Evaluation
-parser.add_argument("--eval-every", type=int, default=500, help="evaluate val bpb every N steps (-1 = disable)")
+parser.add_argument("--eval-every", type=int, default=-1, help="evaluate val bpb every N steps (-1 = disable)")
 parser.add_argument("--eval-tokens", type=int, default=80*524288, help="number of tokens to evaluate val loss on")
 parser.add_argument("--core-metric-every", type=int, default=10000, help="evaluate CORE metric every N steps (-1 = disable)")
 parser.add_argument("--core-metric-max-per-task", type=int, default=500, help="examples per task for CORE metric")
 parser.add_argument("--sample-every", type=int, default=500, help="sample from model every N steps (-1 = disable)")
-parser.add_argument("--save-every", type=int, default=1000, help="save checkpoints every N steps (-1 = only at end)")
+parser.add_argument("--save-every", type=int, default=500, help="save checkpoints every N steps (-1 = only at end)")
 # Output
 parser.add_argument("--model-tag", type=str, default=None, help="override model tag for checkpoint directory name")
 args = parser.parse_args()
